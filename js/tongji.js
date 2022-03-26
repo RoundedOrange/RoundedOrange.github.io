ef4ef0021ecd290f8a8d9fb74aad3eb907b186df
RoundedOrange <RoundedOrange@outlook.com>
2022-03-26 20:38:20 +0800

    Site updated: 2022-03-26 20:38:17

---
+++ js/tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?ef798228610ad40935b19cde596c16e4";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
